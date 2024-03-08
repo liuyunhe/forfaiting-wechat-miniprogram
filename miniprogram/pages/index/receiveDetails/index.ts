db75146ae385357d6def8bdf8739e2e95ef211b0
liuyunhe <liuyunhe666@aliyun.com>
2024-03-08 09:21:41 +0800

    add pages

--- miniprogram/pages/index/receiveDetails/index.ts
+++ miniprogram/pages/index/receiveDetails/index.ts
@@ -1,72 +1,159 @@
+import { addChatUser } from '../../../api/chat-msg/index'
+import { getAssetReleaseDetail } from '../../../api/asset-release/index'
+import { BASE_URL } from '../../../api/request'
+const App = getApp<IAppOption>()
+export {}
 // pages/my/newMarket/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    BASE_URL_IMG: `${BASE_URL}/file/api/file/v1/onlinePreviewController/getFileById_`,
+    id: '',
+    department: '',
+    post: '',
+    avatarFileId: '',
+    releaseUserId: '',
+    releaseUserPhone: '',
+    releaseUserName: '',
+    releaseOrgName: '',
+    releaseOrgCode: '',
+    promiseOrgName: '',
+    promiseOrgTypeName: '',
+    promiseOrgCode: '',
+    promiseOrgType: '',
+    termLower: 0,
+    termUpper: 0,
+    amountLower: 0,
+    amountUpper: 0,
+    rateLower: 0,
+    rateUpper: 0,
+    resellNum: 0,
+    readeName: '',
+    readeType: '30',
+    releaseDeadline: '0',
+    note: '0',
+    userId: App.globalData.userId,
+    orgType:App.globalData.orgType
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const { id } = options
+    this.getDetails(id!)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
+  onShow() {},
 
+  getDetails(id: string) {
+    const params = {
+      releaseId: id
+    }
+    getAssetReleaseDetail(params).then((res) => {
+      if (res.code === '0000') {
+        if (res.value) {
+          const {
+            id,
+            department,
+            post,
+            avatarFileId,
+            releaseUserId,
+            releaseUserPhone,
+            releaseUserName,
+            releaseOrgName,
+            releaseOrgCode,
+            promiseOrgCode,
+            promiseOrgType,
+            termLower,
+            termUpper,
+            amountLower,
+            amountUpper,
+            rateLower,
+            rateUpper,
+            resellNum,
+            readeType,
+            releaseDeadline,
+            note
+          } = res.value
+          
+          this.setData({
+            id,
+            department,
+            post,
+            avatarFileId,
+            releaseUserId,
+            releaseUserPhone,
+            releaseUserName,
+            releaseOrgName,
+            releaseOrgCode,
+            promiseOrgCode,
+            promiseOrgType,
+            termLower,
+            termUpper,
+            amountLower,
+            amountUpper,
+            rateLower,
+            rateUpper,
+            resellNum,
+            readeType,
+            releaseDeadline,
+            note
+          })
+        }
+      } else {
+        App.$showToast(res.message!, 'error')
+      }
+    })
   },
 
-  handleClickChat() { 
-    wx.navigateTo({
-      url:"/pages/index/chatRoom/index"
+  handleClickChat() {
+    const params = {
+      releaseId: this.data.id
+    }
+    addChatUser(params).then((res) => {
+      if (res.code === '0000') {
+        wx.navigateTo({
+          url: `/pages/index/chatRoom/index?id=${res.value}&rUserId=${this.data.releaseUserId}`
+        })
+      } else {
+        App.$showToast(res.message!, 'error')
+      }
     })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {}
 })
