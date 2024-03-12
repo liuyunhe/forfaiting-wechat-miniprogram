ad2d8d927caa5a9529da929b1584fd2251d9ce1a
liuyunhe <liuyunhe666@aliyun.com>
2024-03-12 09:52:47 +0800

    add utils

--- miniprogram/utils/app-config.ts
+++ miniprogram/utils/app-config.ts
@@ -1,14 +1,1 @@
-export const PRI_TMPL_ID_DEV = {
-  SUBSCRIBE_BILL_TMP_ID: "rEHp7goCrg-Qn3pSwCdlD77J7UhLwU8waTSeHldt8GI",
-  PAY_ORDER__TMP_ID: "EjXdGwzdadrZghAFdhXsGhDpzyLtG19GGbdlDoZJXWI",
-  APPOINT_ADD_TMP_ID: "sE_bmwdeCFJXOFxFQoN0TfJow1eOkonIU8ofDVOPFCI"
-}
-export const PRI_TMPL_ID_MASTER = {
-  SUBSCRIBE_BILL_TMP_ID: "-o471KJOd7ZDMWnk2pqKyogInuRvprM7pYjm6Wc4c08",
-  PAY_ORDER__TMP_ID: "E3x-vytcjRsSS0fpqawrPTZWEE5EtP5u9JqAowFKtJE",
-  APPOINT_ADD_TMP_ID: "i7GeCwuv71PdKi2QZiP5vLvKuLOw77_Q9CLrC2IypmE"
-}
-
-export const APPID_DEV = "wx1cd38a01fc4d851c"
-export const APPID_MASTER = "wx892a0790b916498e"
 
--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -7,7 +7,7 @@ export const formatTime = (date: Date) => {
   const second = date.getSeconds()
 
   return (
-    [year, month, day].map(formatNumber).join('/') +
+    [year, month, day].map(formatNumber).join('-') +
     ' ' +
     [hour, minute, second].map(formatNumber).join(':')
   )
@@ -16,4 +16,25 @@ export const formatTime = (date: Date) => {
 const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
+}
+
+export const getDay = (day: number) => {
+
+  const doHandleMonth = (month: number) => {
+    let m = String(month)
+    if (month.toString().length == 1) {
+      m = "0" + month
+    }
+    return m
+  }
+  var today = new Date()
+  var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day
+  today.setTime(targetday_milliseconds)
+
+  var tYear = today.getFullYear()
+  var tMonth = today.getMonth()
+  var tDate = today.getDate()
+  tMonth = Number(doHandleMonth(tMonth + 1))
+  tDate = Number(doHandleMonth(tDate))
+  return tYear + "-" + tMonth + "-" + tDate
 }
